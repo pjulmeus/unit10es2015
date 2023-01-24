cb58b62f52c93a5d2b99eaee788aca7fbe883c82
Paul <pjulmeus@gmail.com>
2023-01-24 05:29:44 -0500

    finished first assignment

---
+++ function-let-var0const.js
@@ -0,0 +1,19 @@
+// let and const Exercise
+// In this exercise, you’ll refactor some ES5 code into ES2015.
+
+ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+//Declare with let
+
+ES2015 Global Constants
+/* Write an ES2015 Version */
+let PI = 3.14;
+PI = 42;
+
+Quiz
+What is the difference between var and let? Var is function scoped, while let is blocked scoped.
+What is the difference between var and const? Var is function scoped and reassignable, while const is block
+and cannot be reassigned.
+What is the difference between let and const? const cannot be reassigned
+What is hoisting? It's when the variable can be declared before the function assigned.
